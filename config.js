c775532ced5acdaa05091824c93e97688531d389
AceLegend <105663319+75gamer@users.noreply.github.com>
2025-04-22 16:32:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,11 +17,11 @@ module.exports = {
   errorLog: "", 
   nodes: [
      {
-      name: "GlaceYT",
-      password: "glaceyt",
-      host: "193.226.78.187",
-      port:  9372,
-      secure: false
+      name: "Ace",
+      password: "oddcoder",
+      host: "lavalink.oddcoder.xyz",
+      port: 443,
+      secure: true
     }
   ]
 }
