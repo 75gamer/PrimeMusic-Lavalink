8fe2f601da1be40afac89655e5a28f6da316651f
AceLegend <105663319+75gamer@users.noreply.github.com>
2025-04-22 19:24:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://acelegend845:<db_password>@musicalcluster.rf7bs0q.mongodb.net/?retryWrites=true&w=majority&appName=MusicalCluster",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
